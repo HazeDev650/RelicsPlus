949b8823db2cd34823107a0e70ce723e45651df2
HazeDev650 <145964153+HazeDev650@users.noreply.github.com>
2023-10-15 12:00:03 -0700

    Update EventListener.php

--- src/Terpz710/RelicsPlus/EventListener/EventListener.php
+++ src/Terpz710/RelicsPlus/EventListener/EventListener.php
@@ -3,47 +3,30 @@
 namespace Terpz710\RelicsPlus\EventListener;
 
 use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\Listener;
-use pocketmine\item\Item;
+use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
-use pocketmine\utils\Config;
-use pocketmine\item\StringToItemParser;
-use pocketmine\item\enchantment\StringToEnchantmentParser;
-use pocketmine\item\enchantment\EnchantmentInstance;
 use Terpz710\RelicsPlus\RelicsManager;
 
 class EventListener implements Listener {
     private $plugin;
     private $relicsManager;
-    private $rewardsConfig;
 
     public function __construct($plugin, RelicsManager $relicsManager) {
         $this->relicsManager = $relicsManager;
         $this->plugin = $plugin;
-        $this->rewardsConfig = new Config($this->plugin->getDataFolder() . "rewards.yml", Config::YAML);
         $this->plugin->getServer()->getPluginManager()->registerEvents($this, $this->plugin);
     }
 
     public function onBlockBreak(BlockBreakEvent $event) {
         $player = $event->getPlayer();
-        $relicRarity = $this->getRandomRelicRarity();
-        if ($relicRarity !== null && $this->chanceToGetRelic($player)) {
-            $relic = $this->relicsManager->createPrismarineRelic($relicRarity); // Pass rarity as an argument
-            $player->getInventory()->addItem($relic);
-            $player->sendMessage("You obtained a $relicRarity relic!");
-        }
-    }
 
-    public function onInteract(PlayerInteractEvent $event) {
-        $player = $event->getPlayer();
-        $item = $event->getItem();
-        
-        // Check if the item is a Prismarine Relic using the RelicsManager method
-        if ($this->relicsManager->isRelic($item->getName())) {
-            $relicRarity = $item->getName();
-            $this->giveReward($player, $relicRarity);
-            $player->getInventory()->removeItem($item);
+        $relicRarity = $this->getRandomRelicRarity();
+
+        if ($relicRarity !== null && $this->chanceToGetRelic($player)) {
+            $relic = $this->relicsManager->createPrismarineRelic($relicRarity);
+            $player->getInventory()->addItem($relic);
+            $player->sendMessage("You obtained a $relicRarity Relic!");
         }
     }
 
@@ -73,37 +56,5 @@ class EventListener implements Listener {
         $chance = 0.01;
 
         return (mt_rand(1, 100) <= $chance * 100);
-    }
-
-    private function giveReward(Player $player, string $relicRarity) {
-        if ($this->rewardsConfig->exists("rewards.$relicRarity")) {
-            $rewards = $this->rewardsConfig->get("rewards.$relicRarity");
-            foreach ($rewards as $rewardData) {
-                $item = $rewardData["item"];
-                $item->setCustomName($rewardData["custom_name"]);
-                $item->setCount($rewardData["quantity"]);
-                if (isset($rewardData["enchantments"])) {
-                    $enchantmentStrings = explode(",", $rewardData["enchantments"]);
-                    $enchantmentInstances = [];
-
-                    foreach ($enchantmentStrings as $enchantmentString) {
-                        $parts = explode(":", $enchantmentString);
-                        if (count($parts) === 2) {
-                            $enchantment = $parts[0];
-                            $level = (int)$parts[1];
-
-                            $enchantmentInstances[] = [$enchantment, $level];
-                        }
-                    }
-
-                    foreach ($enchantmentInstances as $enchantmentInstance) {
-                        $item->addEnchantment($enchantmentInstance[0], $enchantmentInstance[1]);
-                    }
-
-                    $player->getInventory()->addItem($item);
-                    $player->sendMessage("You received a reward: " . $rewardData["custom_name"]);
-                }
-            }
-        }
     }
 }
